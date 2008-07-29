a23e0f84e5f44c2f0f9736c133f40bbc14a83370
Travis Roberts <traviskroberts@gmail.com>
2008-07-29 19:23:18 -0400

    added syntax highlighter script

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+function setup_syntax_highlighting() {
+	dp.SyntaxHighlighter.ClipboardSwf = '/javascripts/syntax_highlighter/clipboard.swf';
+	dp.SyntaxHighlighter.HighlightAll('code');
+}
