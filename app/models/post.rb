a574f202fc8a96cf15acf79fa91e17ffc6bfa2e9
Travis Roberts <traviskroberts@gmail.com>
2009-08-28 23:24:53 -0500

    trying to fix weird ordering with comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
   
   has_and_belongs_to_many :tags
   has_many :comments, :dependent => :destroy
-  has_many :approved_comments, :class_name => 'Comment', :conditions => ["approved = ?", true]
+  has_many :approved_comments, :class_name => 'Comment', :conditions => ["approved = ?", true], :order => 'created_at DESC'
   
   named_scope :featured, :conditions => ["featured = ?", true], :order => 'date DESC'
 	
