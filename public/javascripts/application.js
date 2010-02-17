0dc1db6e816dedf55421a3a46571015744d650b3
Travis Roberts <traviskroberts@gmail.com>
2010-02-16 22:33:45 -0600

    Upgraded syntax highlighter and widened content area.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,2 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-function setup_syntax_highlighting() {
-	dp.SyntaxHighlighter.ClipboardSwf = '/javascripts/syntax_highlighter/clipboard.swf';
-	dp.SyntaxHighlighter.HighlightAll('code');
-}
