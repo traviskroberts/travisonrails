6f79100a54e0df24c55c497c2b4611f8d3acd101
Travis Roberts <traviskroberts@gmail.com>
2011-08-21 21:26:37 -0500

    Preparing for deploy.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,10 +1,10 @@
-require 'railsmachine/recipes'
+require "bundler/capistrano"
 
 # =============================================================================
 # REQUIRED VARIABLES
 # =============================================================================
 # The name of your application. Used for directory and file names
-set :application, "tor_blog"
+set :application, "travisonrails"
 
 # Target directory for the application on the web and app servers.
 set :deploy_to, "/var/www/apps/#{application}"
@@ -14,11 +14,7 @@ set :domain, "travisonrails.com"
 
 # Login user for ssh.
 set :user, "deploy"
-set :runner, user
-set :admin_runner, user
-
-# Rails environment. Used by application setup tasks and migrate tasks.
-set :rails_env, "production"
+set :use_sudo, false
 
 # =============================================================================
 # ROLES
@@ -29,36 +25,25 @@ role :app, domain
 role :db,  domain, :primary => true
 
 # =============================================================================
-# APPLICATION SERVER OPTIONS
-# ============================================================================= 
-set :app_server, :passenger  # :mongrel or :passenger
-
-# =============================================================================
-# DATABASE OPTIONS
-# =============================================================================
-set :database, "mysql"   # mysql or postgresql
-
-# =============================================================================
 # SCM OPTIONS
 # =============================================================================
 set :scm, :git
-set :repository, "git@github.com:travisr/#{application}.git"
+set :repository, "git@github.com:travisr/tor_blog.git"
+set :branch, :master
 
 # =============================================================================
 # CAPISTRANO OPTIONS
 # =============================================================================
-set :keep_releases, 5
+set :keep_releases, 3
 set :deploy_via, :remote_cache
 
 # action to symlink database file
 namespace :deploy do
   desc "Symlink database config file."
   task :symlink_db do
-    run "ln -nfs #{shared_path}/system/database.yml #{release_path}/config/database.yml"
-    
-    # also need to symlink carpguy folder :)
-    run "ln -nfs #{shared_path}/system/carpguy #{release_path}/public/carpguy"
+    run "ln -nfs #{shared_path}/database.yml #{release_path}/config/database.yml"
   end
 end
- 
+
 after 'deploy:update_code', 'deploy:symlink_db'
+after 'deploy:symlink', 'deploy:cleanup'
