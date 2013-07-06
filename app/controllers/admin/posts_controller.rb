dc6b50727b27efae204d1f499ff9c4dfb063fcd0
Travis Roberts <traviskroberts@gmail.com>
2013-07-06 15:48:57 -0500

    Removed unnecessary code in admin controller.

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -26,83 +26,48 @@ class Admin::PostsController < ApplicationController
 
   def index
     @posts = Post.order('posts.date DESC').paginate(:page => params[:page], :per_page => 10)
-    respond_to do |format|
-      format.html # index.html.erb
-    end
-  rescue Exception => ex
-    logger.warn('ERROR: ' + ex.message)
-    flash.now[:error] = 'There was an error getting the list of posts.'
   end
 
   def search
-    @posts = Post.where('title LIKE ?', "%#{params[:query]}%").paginate(:page => params[:page], :per_page => 10)
-    respond_to do |format|
-      format.html { render :action => 'index' }
-    end
+    @posts = Post.where('title ILIKE ?', "%#{params[:query]}%").paginate(:page => params[:page], :per_page => 10)
+    render :action => 'index'
   end
 
   def show
     @post = Post.find(params[:id])
-    respond_to do |format|
-      format.html # show.html.erb
-    end
-  rescue Exception => ex
-    logger.warn('ERROR: ' + ex.message)
-    flash.now[:error] = 'There was an error getting the requested post.'
   end
 
   def new
     @post = Post.new
-    respond_to do |format|
-      format.html # new.html.erb
-    end
-  rescue Exception => ex
-    logger.warn('ERROR: ' + ex.message)
-    flash.now[:error] = 'There was an error creating the post.'
   end
 
   def edit
     @post = Post.find(params[:id])
     get_tags(@post)
-  rescue Exception => ex
-    logger.warn('ERROR: ' + ex.message)
-    flash.now[:error] = 'There was an error editing the post.  The post could not be found.'
   end
 
   def create
     @post = Post.new(params[:post])
     save_tags(params[:tags], @post)
-    respond_to do |format|
-      if @post.save
-        flash[:notice] = 'Post was successfully created.'
-        format.html { redirect_to admin_posts_path }
-      else
-        flash.now[:error] = 'The post could not be created'
-        format.html { render :action => 'new' }
-      end
+    if @post.save
+      flash[:notice] = 'Post was successfully created.'
+      redirect_to admin_posts_path
+    else
+      flash.now[:error] = 'The post could not be created'
+      render :action => 'new'
     end
-  rescue Exception => ex
-    logger.warn('ERROR: ' + ex.message)
-    flash.now[:error] = 'There was an error creating the post.'
-    render :action => 'new'
   end
 
   def update
     @post = Post.find(params[:id])
     save_tags(params[:tags], @post)
-    respond_to do |format|
-      if @post.update_attributes(params[:post])
-        flash[:notice] = 'Post was successfully updated.'
-        format.html { redirect_to admin_posts_path }
-      else
-        flash.now[:error] = 'The post could not be updated.'
-        format.html { render :action => 'edit' }
-      end
+    if @post.update_attributes(params[:post])
+      flash[:notice] = 'Post was successfully updated.'
+      redirect_to admin_posts_path
+    else
+      flash.now[:error] = 'The post could not be updated.'
+      render :action => 'edit'
     end
-  rescue Exception => ex
-    logger.warn('ERROR: ' + ex.message)
-    flash.now[:error] = 'There was an error updating the post.'
-    render :action => 'edit'
   end
 
   def destroy
@@ -112,9 +77,6 @@ class Admin::PostsController < ApplicationController
       format.html { redirect_to admin_posts_path }
       format.js { render(:update) { |page| page.remove dom_id(@post) } }
     end
-  rescue Exception => ex
-    logger.warn('ERROR: ' + ex.message)
-    render :nothing => true
   end
 
   private
