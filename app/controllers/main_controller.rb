869aa15fc82cce3fd4919a96745a8948de516bc4
Travis Roberts <traviskroberts@gmail.com>
2013-07-06 13:06:37 -0500

    Updated activerecord queries to work with postgres.

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,51 +3,39 @@ class MainController < ApplicationController
 
   def index
     if params[:search].present?
-      @posts = Post.where("title LIKE ?", "%#{params[:search]}%").order('posts.date DESC').includes(:tags).paginate(:page => params[:page], :per_page => 5)
+      @posts = Post.where("title ILIKE ?", "%#{params[:search]}%").order('posts.date DESC').includes(:tags).paginate(:page => params[:page], :per_page => 5)
     else
       @posts = Post.order('posts.date DESC').includes(:tags).paginate(:page => params[:page], :per_page => 5)
     end
-  rescue Exception => ex
-    logger.warn("ERROR: " + ex.message)
-    flash.now[:error] = 'There was an error getting the latest posts.'
   end
 
   def by_date
-    if params[:day]
-      @query = "#{params[:year]}-#{params[:month]}-#{params[:day]}%"
-    elsif params[:month]
-      @query = "#{params[:year]}-#{params[:month]}%"
-    elsif params[:year]
-      @query = "#{params[:year]}%"
-    else
-      flash[:error] = "I did not understand your request."
-      redirect_to home_path
-    end
-    # now get the post(s) for that date
+    # get the post(s) for that date
     if params[:slug]
       @post = Post.where("slug = ?", params[:slug]).first
     else
-      @posts = Post.where("posts.date LIKE ?", "#{@query}%").order('posts.date').includes(:tags)
+      if params[:day]
+        @posts = Post.where('posts.date = ?', "#{params[:year]}-#{params[:month]}-#{params[:day]}")
+      elsif params[:month]
+        @posts = Post.where("date_part('year', posts.date) = ? AND date_part('month', posts.date) = ?", params[:year], params[:month])
+      elsif params[:year]
+        @posts = Post.where("date_part('year', posts.date) = ?", params[:year])
+      else
+        flash[:error] = "I did not understand your request."
+        redirect_to home_path
+      end
+      @posts = @posts.order('posts.date').includes(:tags)
     end
-  rescue Exception => ex
-    logger.warn("ERROR: " + ex.message)
-    flash.now[:error] = 'There was an error getting the requested post(s).'
   end
 
   def tagged
     tag_name = params[:name].gsub('-',' ')
     @tag = Tag.find_by_name(tag_name)
     @posts = @tag.posts.order('date DESC').paginate(:page => params[:page], :per_page => 5) unless @tag.nil?
-  rescue Exception => ex
-    logger.warn("ERROR: " + ex.message)
-    flash.now[:error] = 'There was an error getting the posts for that tag.'
   end
 
   def feed
     @posts = Post.order('date DESC').limit(10)
-  rescue Exception => ex
-    logger.warn("ERROR: " + ex.message)
-    flash.now[:error] = 'There was an error generating the feed.'
   end
 
   # routes to fix old links
@@ -81,8 +69,8 @@ class MainController < ApplicationController
       build_date(Date.today)
       @month = @month.to_i
 
-      #get all posts of the current year
-      @results = Post.where("date LIKE '#{@cur_year}%'").order('date DESC')
+      # get all posts of the current year
+      @results = Post.where("date_part('year', date) = ?",  @cur_year).order('date DESC')
       if @results and @results.length > 0
         @results.each do |p|
           #if the month of the post is less than the current month, make a link to it
@@ -93,17 +81,17 @@ class MainController < ApplicationController
             else
               @link_month = p.date.month.to_s
             end
-            #generate the link's name and the link's URL
+            # generate the link's name and the link's URL
             name = get_month_name(@link_month.to_i) + " " + @cur_year.to_s
             linkage = @cur_year.to_s + "/" + @link_month
-            #add the archive link to the @links array if it's not already in
+            # add the archive link to the @links array if it's not already in
             @links.push([name, linkage]) unless @links.include?([name, linkage])
           end
         end
       end
 
       #find all the posts that are not from the current year
-      @results = Post.where("date NOT LIKE '#{@cur_year}%'").order('date DESC')
+      @results = Post.where("date_part('year', date) != ?", @cur_year).order('date DESC')
       if @results and @results.length > 0
         @results.each do |p|
           #generate the link's name and the link's URL
