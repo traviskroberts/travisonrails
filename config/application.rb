544ed2e851ec80fcc93ac68c4129dc026c71dcef
Travis Roberts <traviskroberts@gmail.com>
2013-07-06 13:29:29 -0500

    Turn off app initialize when compiling assets.

--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,7 @@ module Travisonrails
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
+    config.assets.initialize_on_precompile = false
     config.assets.paths << Rails.root.join("vendor", "assets", "fonts")
   end
 end
